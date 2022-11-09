9f7c38455796780b1f9d0859f125f54924fb8df1
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-09 21:33:45 +0530

    Added Arpan Website...Coming Soon.....

--- frontend/src/components/Home/OtherEvents.jsx
+++ frontend/src/components/Home/OtherEvents.jsx
@@ -4,15 +4,22 @@ function OtherEvents() {
       <h1 className="title">Other Events</h1>
       <div className="event-list">
         <div className="other-events">
-          <img src="https://source.unsplash.com/random?mountain" alt="Arpan" />
-          <h2>Lorem, ipsum.</h2>
+          <div className="arpan">
+            <img src="Arpan.png" alt="Arpan" />
+          </div>
+          <h2>Arpan</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea impedit
             et beatae sint saepe fugit.
           </p>
         </div>
         <div className="other-events">
-          <img src="https://source.unsplash.com/random?mountain" alt="Arpan" />
+          <div>
+            <img
+              src="https://source.unsplash.com/random?mountain"
+              alt="Arpan"
+            />
+          </div>
           <h2>Lorem, ipsum.</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea impedit
