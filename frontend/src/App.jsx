a910a13a593b5e13b846697eb57ad8878e1df6a0
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-12 22:41:58 +0530

    Added Skeletal Loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,45 @@ import HomePage from "./pages/HomePage"
 import Socials from "./components/UI Components/Socials/Socials"
 import Navbar from "./components/UI Components/Navbar/Navbar"
 import EventPage from "./pages/EventPage"
+import { useState } from "react"
 import Background from "./components/UI Components/Background/Background"
+import Loading from "./components/Loading/Loading"
 import "./App.css"
 
 function App() {
+  // Set loading to true if loading screen is needed else set it to false
+  const [loading, setLoading] = useState(true)
+  const LOADER_TIMER = 5000
+  const [activate, setActivate] = useState(false)
+
+  const websiteStart = () => {
+    setTimeout(() => {
+      setLoading(false)
+    }, LOADER_TIMER)
+
+    setActivate(true)
+  }
+
   return (
     <div className="container">
       <Background>
         <Router>
-          <Socials />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/events" element={<EventPage />} />
-          </Routes>
+          {loading ? (
+            <Loading
+              onClick={websiteStart}
+              activate={activate}
+              timer={LOADER_TIMER}
+            />
+          ) : (
+            <>
+              <Socials />
+              <Navbar />
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/events" element={<EventPage />} />
+              </Routes>
+            </>
+          )}
         </Router>
       </Background>
     </div>
---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import "./Loading.css"
+
+function Loading({ onClick, activate, timer }) {
+  const [count, setCount] = useState(timer / 1000)
+  if (activate) {
+    setTimeout(() => {
+      setCount(() => count - 1)
+    }, 1000)
+  }
+  return (
+    <div id="loader">
+      {activate ? (
+        <div className="loader-content">
+          <h1>Clouds Initializing</h1>
+          <p>{count}</p>
+        </div>
+      ) : (
+        ""
+      )}
+      {activate && <span class="loader"></span>}
+      {!activate && (
+        <button onClick={onClick} className="activate-btn">
+          Initialize Clouds
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default Loading
