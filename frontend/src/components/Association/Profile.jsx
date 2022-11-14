7113ade1b5235e99f2c9292041c7f06377387cfe
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-14 21:05:24 +0530

    Minor-less-Major fix

--- frontend/src/components/Association/Profile.jsx
+++ frontend/src/components/Association/Profile.jsx
@@ -1,10 +1,7 @@
 function Profile({ img, name, role, extras, socials }) {
   return (
     <div className="profile">
-      <a
-        href="https://www.linkedin.com/in/deveesh-shetty-908539214/"
-        target="_blank"
-      >
+      <a href={socials} target="_blank">
         <img src={img} alt={role ? `${name} - ${role}` : name} loading="lazy" />
       </a>
       <div>
