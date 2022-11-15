8e4c7ab7681c6558677dca5afd0866d4575e6a36
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-15 20:27:59 +0530

    Updated 1 image and removed filler event

--- frontend/src/components/Events/events.js
+++ frontend/src/components/Events/events.js
@@ -1,31 +1,14 @@
-const events = [
-  {
-    id: 1,
-    img: "https://source.unsplash.com/random?computer",
-    title: "Lorem ipsum dolor sit amet.",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam dignissimos eos, odit repudiandae quas. Soluta iure quos quis possimus. A sint explicabo sit blanditiis, ipsum debitis officiis minus laudantium nemo minima. In, quae et? Fuga, incidunt. Reiciendis neque eos autem sequi quos rerum omnis quisquam veniam delectus non. Laboriosam!",
-    time: "12-9-2003",
-    venue: "Sahyadri College of Engineering and Management",
-  },
-  {
-    id: 2,
-    img: "https://source.unsplash.com/random?computer",
-    title: "Lorem ipsum dolor sit amet.",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam dignissimos eos, odit repudiandae quas. Soluta iure quos quis possimus. A sint explicabo sit blanditiis, ipsum debitis officiis minus laudantium nemo minima. In, quae et? Fuga, incidunt. Reiciendis neque eos autem sequi quos rerum omnis quisquam veniam delectus non. Laboriosam!",
-    time: "12-9-2003",
-    venue: "Sahyadri College of Engineering and Management",
-  },
-  {
-    id: 3,
-    img: "https://source.unsplash.com/random?computer",
-    title: "Lorem ipsum dolor sit amet.",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam dignissimos eos, odit repudiandae quas. Soluta iure quos quis possimus. A sint explicabo sit blanditiis, ipsum debitis officiis minus laudantium nemo minima. In, quae et? Fuga, incidunt. Reiciendis neque eos autem sequi quos rerum omnis quisquam veniam delectus non. Laboriosam!",
-    time: "12-9-2003",
-    venue: "Sahyadri College of Engineering and Management",
-  },
-]
+// Schema
+// {
+//   id: 1,
+//   img: "https://source.unsplash.com/random?computer",
+//   title: "Lorem ipsum dolor sit amet.",
+//   description:
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa laboriosam dignissimos eos, odit repudiandae quas. Soluta iure quos quis possimus. A sint explicabo sit blanditiis, ipsum debitis officiis minus laudantium nemo minima. In, quae et? Fuga, incidunt. Reiciendis neque eos autem sequi quos rerum omnis quisquam veniam delectus non. Laboriosam!",
+//   time: "12-9-2003",
+//   venue: "Sahyadri College of Engineering and Management",
+// },
+
+const events = []
 
 export default events
