3df342ff4b3d3496164fdfc192d282d58b40ae6f
Rohan <rohankumar5803@gmail.com>
2023-12-05 23:00:52 +0530

    Developer.js updated

--- frontend/src/components/Association/Roles/Developer.jsx
+++ frontend/src/components/Association/Roles/Developer.jsx
@@ -7,19 +7,19 @@ function Developer() {
         <p className="sub-title">Web Developers</p>
         <div className="section divide-3">
           <Profile
-            img="Profile/Deveesh.jpeg"
-            name="Deveesh C Shetty"
-            socials="https://www.linkedin.com/in/deveesh-shetty-908539214/"
+            img="Profile/DeveeshShetty.jpg"
+            name="Deveesh Shetty"
+            socials="https://www.linkedin.com/in/deveesh-shetty-908539214"
           />
           <Profile
             img="Profile/Tejas.jpg"
             name="Tejas G K"
-            socials="https://www.linkedin.com/in/tejas-gk/"
+            socials="https://www.linkedin.com/in/tejas-gk"
           />
           <Profile
-            img="Profile/Rohan.jpg"
+            img="Profile/Rohan.JPG"
             name="Rohan"
-            socials="https://www.linkedin.com/in/rohan-44aa02228/"
+            socials="https://www.linkedin.com/in/rohan-44aa02228"
           />
         </div>
       </div>
