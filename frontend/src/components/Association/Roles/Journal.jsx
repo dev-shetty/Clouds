4ca490c8c8fe94cc70c5704592db8eb5e3776c5e
Rohan <rohankumar5803@gmail.com>
2023-12-05 23:07:15 +0530

    Journal.jsx updated

--- frontend/src/components/Association/Roles/Journal.jsx
+++ frontend/src/components/Association/Roles/Journal.jsx
@@ -5,16 +5,12 @@ function Journal() {
     <div>
       <div>
         <p className="sub-title">Journal Secretary</p>
-        <div className="section divide-2">
-          <Profile img="Profile/Sucheta.jpg" name="Sucheta S Shanbhag" />
-          <Profile img="Profile/VinolDSouza.jpg" name="Vinol Chris D Souza" />
-        </div>
-      </div>
-      <div>
-        <p className="sub-title">Joint Journal Secretary</p>
-        <div className="section divide-2">
-          <Profile img="Profile/Amritha.jpg" name="Amritha" />
-          <Profile img="Profile/LakshmiAkshayaT.jpg" name="Lakshmi Akshaya T" />
+        <div className="section divide-1">
+          <Profile 
+            img="Profile/" 
+            name="Shreya Sudhakaran" 
+            socials=""
+          />
         </div>
       </div>
     </div>
