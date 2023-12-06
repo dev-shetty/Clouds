82a2b909b699395ffcaa2c9078d885af5fe7a679
Rohan <rohankumar5803@gmail.com>
2023-12-06 11:10:17 +0530

    updated Journal.jsx

--- frontend/src/components/Association/Roles/Journal.jsx
+++ frontend/src/components/Association/Roles/Journal.jsx
@@ -5,11 +5,17 @@ function Journal() {
     <div>
       <div>
         <p className="sub-title">Journal Secretary</p>
-        <div className="section divide-1">
+        <div className="section divide-2">
           <Profile 
             img="Profile/ShreyaSudhakaran.jpeg" 
             name="Shreya Sudhakaran" 
             socials=""
+          />
+          <Profile 
+            img="Profile/SoujanyaRao.jpg" 
+            name="Soujanya Rao" 
+            role="Joint Journal Secretary"
+            socials="https://www.linkedin.com/in/soujanya-rao-3b5150229"
           />
         </div>
       </div>
